f75994a406dc22ae4e3c71e5d83b7aebf1f7551d
gunom <gu3062@naver.com>
2023-04-17 19:11:48 +0900

    [FEAT] backery - 태그 모달 구현

--- week2/backery/index.js
+++ week2/backery/index.js
@@ -16,6 +16,14 @@ products.forEach((product) => {
         <ul class="card-tags">
         </ul>
         <button class="card-tags-add">+</button>
+        <div class="modal">
+        <div class="modal-content">
+            <span class="close-btn">&times;</span>
+            <ul class="modal-tags">
+            </ul>
+        </div>
+        </div>
+    </div>
     </div>
     <img src="${product.image}" alt="상품이미지" />
     <button class="card-like">
@@ -27,6 +35,34 @@ products.forEach((product) => {
         tagElement.classList.add('card-tag');
         tagElement.textContent = tag;
         productElement.querySelector('.card-tags').appendChild(tagElement);
+    });
+    product.tag.forEach((tag) => {
+        const tagElement = document.createElement('li');
+        tagElement.classList.add('modal-tag');
+        tagElement.textContent = tag;
+        productElement.querySelector('.modal-tags').appendChild(tagElement);
+    });
+    productsContainer.addEventListener('click', (event) => {
+        const addButton = event.target.closest('.card-tags-add');
+        if (addButton) {
+            const productElement = addButton.closest('.card');
+            const modal = productElement.querySelector('.modal');
+            modal.style.display = "block";
+            const buttonRect = addButton.getBoundingClientRect();
+            const modalTop = buttonRect.top - modal.offsetHeight;
+            const modalLeft = buttonRect.right - (modal.offsetWidth / 2);
+            modal.style.top = `${modalTop}px`;
+            modal.style.left = `${modalLeft}px`;
+            modal.classList.add('show');
+        }
+    });
+    productsContainer.addEventListener('click', (event) => {
+        const closeButton = event.target.closest('.close-btn');
+        if (closeButton) {
+            const modal = closeButton.closest('.modal');
+            modal.style.display = "none";
+            modal.classList.remove('show');
+        }
     });
     productsContainer.appendChild(productElement);
 });
@@ -93,13 +129,21 @@ const displayProducts = () => {
         productElement.innerHTML = `
         <h2 class="card-title">${product.name}</h2>
         <div class="card-tagwrapper">
-          <ul class="card-tags">
-          </ul>
-          <button class="card-tags-add">+</button>
+            <ul class="card-tags">
+            </ul>
+            <button class="card-tags-add">+</button>
+            <div class="modal">
+            <div class="modal-content">
+                <span class="close-btn">&times;</span>
+                <ul class="modal-tags">
+                </ul>
+            </div>
+            </div>
+        </div>
         </div>
         <img src="${product.image}" alt="상품이미지" />
         <button class="card-like">
-          <i class="fa fa-heart"></i>
+            <i class="fa fa-heart"></i>
         </button>
         `;
         product.tag.forEach((tag) => {
@@ -107,6 +151,34 @@ const displayProducts = () => {
             tagElement.classList.add('card-tag');
             tagElement.textContent = tag;
             productElement.querySelector('.card-tags').appendChild(tagElement);
+        });
+        product.tag.forEach((tag) => {
+            const tagElement = document.createElement('li');
+            tagElement.classList.add('modal-tag');
+            tagElement.textContent = tag;
+            productElement.querySelector('.modal-tags').appendChild(tagElement);
+        });
+        productsContainer.addEventListener('click', (event) => {
+            const addButton = event.target.closest('.card-tags-add');
+            if (addButton) {
+                const productElement = addButton.closest('.card');
+                const modal = productElement.querySelector('.modal');
+                modal.style.display = "block";
+                const buttonRect = addButton.getBoundingClientRect();
+                const modalTop = buttonRect.top - modal.offsetHeight;
+                const modalLeft = buttonRect.right - (modal.offsetWidth / 2);
+                modal.style.top = `${modalTop}px`;
+                modal.style.left = `${modalLeft}px`;
+                modal.classList.add('show');
+            }
+        });
+        productsContainer.addEventListener('click', (event) => {
+            const closeButton = event.target.closest('.close-btn');
+            if (closeButton) {
+                const modal = closeButton.closest('.modal');
+                modal.style.display = "none";
+                modal.classList.remove('show');
+            }
         });
         productsContainer.appendChild(productElement);
     });
